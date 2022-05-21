c55cf4c3d03260e07804167e9fabf667c32ddff6
aman.singh5 <aman.singh@cillabs.com>
2022-05-22 01:15:24 +0530

    change in api

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,70 +5,70 @@
 // }
 
 //action types
-export const ADD_MOVIES = 'ADD_MOVIES'; 
-export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
-export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
-export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
-export const ADD_MOVIE_TO_LIST = 'ADD_MOVIE_TO_LIST';
-export const ADD_SEARCH_RESULT = 'ADD_SEARCH_RESULT';
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
+export const ADD_MOVIE_TO_LIST = "ADD_MOVIE_TO_LIST";
+export const ADD_SEARCH_RESULT = "ADD_SEARCH_RESULT";
 
 //action creators
-export function addMovies (movies) {
-    return {
-        type: ADD_MOVIES,
-        movies
-    }
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
 }
 
-export function addFavourite (movie) {
-    return {
-        type: ADD_TO_FAVOURITES,
-        movie
-    }
+export function addFavourite(movie) {
+  return {
+    type: ADD_TO_FAVOURITES,
+    movie,
+  };
 }
 
-export function removeFromFavourites (movie) {
-    return {
-        type: REMOVE_FROM_FAVOURITES,
-        movie
-    }
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie,
+  };
 }
 
-export function setShowFavourites (val) {
-    return {
-        type: SET_SHOW_FAVOURITES,
-        val
-    }
+export function setShowFavourites(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val,
+  };
 }
 
 export function addMovieToList(movie) {
-    return {
-        type: ADD_MOVIE_TO_LIST,
-        movie
-    }
+  return {
+    type: ADD_MOVIE_TO_LIST,
+    movie,
+  };
 }
 
 export function handleMovieSearch(searchText) {
-    // backtiks(i.e ``) are used when we want to replace the asctual data from the server
-    //In this case url will be replaced by the movie which will be fetched using api call
+  // backtiks(i.e ``) are used when we want to replace the asctual data from the server
+  //In this case url will be replaced by the movie which will be fetched using api call
 
-    const url = `http://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
+  const url = `https://www.omdbapi.com/?apikey=3ca5df7&t=${searchText}`;
 
-    return function (dispatch) {
-        fetch(url)
-        .then(response => response.json())
-        .then(movie => {
-            console.log('movie', movie);
+  return function (dispatch) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((movie) => {
+        console.log("movie", movie);
 
-            // dispatch an action
-             dispatch(addMovieSearchResult(movie));
-        })
-    }
+        // dispatch an action
+        dispatch(addMovieSearchResult(movie));
+      });
+  };
 }
 
 export function addMovieSearchResult(movie) {
-    return {
-        type: ADD_SEARCH_RESULT,
-        movie
-    }
+  return {
+    type: ADD_SEARCH_RESULT,
+    movie,
+  };
 }
